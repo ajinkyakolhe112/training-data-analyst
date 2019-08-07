6456ece4978ec6cc6a37713c58c0dd5860a98b39
Ajinkya Kolhe <ajinkyakolhe112@gmail.com>
2019-08-07 19:35:14 +0000

    model life cycle v2 refractor

--- data-engineering-1.x/4_AI-Platform_Machine_Learning/Demo/taxifare/trainer/model_life_cycle_complete.py
+++ data-engineering-1.x/4_AI-Platform_Machine_Learning/Demo/taxifare/trainer/model_life_cycle_complete.py
@@ -7,56 +7,74 @@ print(tf.__version__)
 tf.logging.set_verbosity(tf.logging.INFO)
 
 # In CSV, label is the first column, after the features, followed by the key
-CSV_COLUMNS = [
-    "fare_amount",
-    "pickuplon",
-    "pickuplat",
-    "dropofflon",
-    "dropofflat",
-    "passengers",
-    "key",
-]
-FEATURES = CSV_COLUMNS[1 : len(CSV_COLUMNS) - 1]
+CSV_COLUMNS = ['fare_amount', 'pickuplon', 'pickuplat', 'dropofflon', 'dropofflat', 'passengers', 'key']
+FEATURES = CSV_COLUMNS[1:len(CSV_COLUMNS) - 1]
 LABEL = CSV_COLUMNS[0]
+DEFAULTS = [[0.0], [-74.0], [40.0], [-74.0], [40.7], [1.0], ['nokey']]
 
 
-# Create an input function that stores your data
-def make_input_fn(df, num_epochs):
-    return tf.estimator.inputs.pandas_input_fn(
-        x=df,
-        y=df[LABEL],
-        batch_size=128,
-        num_epochs=num_epochs,
-        shuffle=True,
-        queue_capacity=1000,
-        num_threads=1,
-    )
+# Create an input function that stores your data into a dataset
+def read_dataset(filename, mode, batch_size=512):
+    def _input_fn():
+        def decode_csv(value_column):
+            columns = tf.decode_csv(value_column, record_defaults=DEFAULTS)
+            features = dict(zip(CSV_COLUMNS, columns))
+            label = features.pop(LABEL_COLUMN)
+            return features, label
+
+        # Create list of files that match pattern
+        file_list = tf.gfile.Glob(filename)
+
+        # Create dataset from file list
+        dataset = tf.data.TextLineDataset(file_list).map(decode_csv)
+
+        if mode == tf.estimator.ModeKeys.TRAIN:
+            num_epochs = None  # indefinitely
+            dataset = dataset.shuffle(buffer_size=10 * batch_size)
+        else:
+            num_epochs = 1  # end-of-input after this
+
+        dataset = dataset.repeat(num_epochs).batch(batch_size)
+        return dataset.make_one_shot_iterator().get_next()
+
+    return _input_fn
 
 
+# Define your feature columns
 def make_feature_cols():
-    input_columns = [tf.feature_column.numeric_column(k) for k in FEATURES]
-    return input_columns
+    INPUT_COLUMNS = [
+        tf.feature_column.numeric_column('pickuplon'),
+        tf.feature_column.numeric_column('pickuplat'),
+        tf.feature_column.numeric_column('dropofflat'),
+        tf.feature_column.numeric_column('dropofflon'),
+        tf.feature_column.numeric_column('passengers'),
+    ]
+
+    def add_more_features(feats):
+        # Nothing to add (yet!)
+        return feats
+
+    all_feature_cols = add_more_features(INPUT_COLUMNS)
+
+    return all_feature_cols
 
 
 shutil.rmtree(OUTDIR, ignore_errors=True)  # start fresh each time
 
-model = tf.estimator.LinearRegressor(feature_columns=feature_cols, model_dir=OUTDIR)
-
-model.train(
-    input_fn=make_input_fn(
-        pd.read_csv("./taxi-train.csv", header=None, names=CSV_COLUMNS), num_epochs=100
-    )
+model = tf.estimator.LinearRegressor(
+    feature_columns=feature_cols,
+    model_dir=OUTDIR
 )
 
+model.train(
+    input_fn=read_dataset('./taxi-train.csv', mode=tf.estimator.ModeKeys.TRAIN),num_epochs=100)
+)
 
 def print_rmse(model, name, df):
     metrics = model.evaluate(
-        input_fn=make_input_fn(
-            pd.read_csv("./taxi-valid.csv", header=None, names=CSV_COLUMNS),
-            num_epochs=1,
-        )
+        input_fn=read_dataset('./taxi-valid.csv', mode=tf.estimator.ModeKeys.EVAL)
     )
-    print("RMSE on {} dataset = {}".format(name, np.sqrt(metrics["average_loss"])))
+    print('RMSE on {} dataset = {}'.format(name, np.sqrt(metrics['average_loss'])))
 
 
-print_rmse(model, "validation", df_valid)
+print_rmse(model, 'validation', df_valid)
