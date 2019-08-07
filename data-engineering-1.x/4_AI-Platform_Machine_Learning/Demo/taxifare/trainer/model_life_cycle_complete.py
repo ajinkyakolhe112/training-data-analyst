adcb4355e276a83a1a4f879deee4e9aa9e284a44
Ajinkya Kolhe <ajinkyakolhe112@gmail.com>
2019-08-07 20:26:34 +0000

    model v1

--- data-engineering-1.x/4_AI-Platform_Machine_Learning/Demo/taxifare/trainer/model_life_cycle_complete.py
+++ data-engineering-1.x/4_AI-Platform_Machine_Learning/Demo/taxifare/trainer/model_life_cycle_complete.py
@@ -10,94 +10,45 @@ tf.logging.set_verbosity(tf.logging.INFO)
 CSV_COLUMNS = ['fare_amount', 'pickuplon', 'pickuplat', 'dropofflon', 'dropofflat', 'passengers', 'key']
 FEATURES = CSV_COLUMNS[1 : len(CSV_COLUMNS) - 1]
 LABEL = CSV_COLUMNS[0]
-DEFAULTS = [[0.0], [-74.0], [40.0], [-74.0], [40.7], [1.0], ["nokey"]]
 
 
-# Create an input function that stores your data into a dataset
-def read_dataset(filename, mode, batch_size=512):
-    def _input_fn():
-        def decode_csv(value_column):
-            columns = tf.decode_csv(value_column, record_defaults=DEFAULTS)
-            features = dict(zip(CSV_COLUMNS, columns))
-            label = features.pop(LABEL)
-            return features, label
-
-        # Create list of files that match pattern
-        file_list = tf.gfile.Glob(filename)
-
-        # Create dataset from file list
-        dataset = tf.data.TextLineDataset(file_list).map(decode_csv)
-
-        if mode == tf.estimator.ModeKeys.TRAIN:
-            num_epochs = None  # indefinitely
-            dataset = dataset.shuffle(buffer_size=10 * batch_size)
-        else:
-            num_epochs = 1  # end-of-input after this
-
-        dataset = dataset.repeat(num_epochs).batch(batch_size)
-        return dataset.make_one_shot_iterator().get_next()
-
-    return _input_fn
+# Create an input function that stores your data
+def make_input_fn(df, num_epochs):
+    return tf.estimator.inputs.pandas_input_fn(
+        x=df,
+        y=df[LABEL],
+        batch_size=128,
+        num_epochs=num_epochs,
+        shuffle=True,
+        queue_capacity=1000,
+        num_threads=1,
+    )
 
 
-# Define your feature columns
 def make_feature_cols():
-    INPUT_COLUMNS = [
-        tf.feature_column.numeric_column("pickuplon"),
-        tf.feature_column.numeric_column("pickuplat"),
-        tf.feature_column.numeric_column("dropofflat"),
-        tf.feature_column.numeric_column("dropofflon"),
-        tf.feature_column.numeric_column("passengers"),
-    ]
-
-    def add_more_features(feats):
-        # Nothing to add (yet!)
-        return feats
-
-    all_feature_cols = add_more_features(INPUT_COLUMNS)
-
-    return all_feature_cols
-
-
-# Create your serving input function so that your trained model will be able to serve predictions
-def serving_input_fn():
-    feature_placeholders = {
-        "pickuplon": tf.placeholder(tf.float32, [None]),
-        "pickuplat": tf.placeholder(tf.float32, [None]),
-        "dropofflat": tf.placeholder(tf.float32, [None]),
-        "dropofflon": tf.placeholder(tf.float32, [None]),
-        "passengers": tf.placeholder(tf.float32, [None]),
-    }
-    features = {
-        key: tf.expand_dims(tensor, -1) for key, tensor in feature_placeholders.items()
-    }
-    return tf.estimator.export.ServingInputReceiver(features, feature_placeholders)
-
-
-# Create an estimator that we are going to train and evaluate
-def train_and_evaluate(output_dir, num_train_steps):
-    estimator = tf.estimator.LinearRegressor(
-        model_dir=output_dir, feature_columns=make_feature_cols()
-    )
-
-    train_spec = tf.estimator.TrainSpec(
-        input_fn=read_dataset("./taxi-train.csv", mode=tf.estimator.ModeKeys.TRAIN),
-        max_steps=num_train_steps,
-    )
-
-    exporter = tf.estimator.LatestExporter("exporter", serving_input_fn)
-
-    eval_spec = tf.estimator.EvalSpec(
-        input_fn=read_dataset("./taxi-valid.csv", mode=tf.estimator.ModeKeys.EVAL),
-        steps=None,
-        start_delay_secs=1,  # start evaluating after N seconds
-        throttle_secs=10,  # evaluate every N seconds
-        exporters=exporter,
-    )
-
-    tf.estimator.train_and_evaluate(estimator, train_spec, eval_spec)
+    input_columns = [tf.feature_column.numeric_column(k) for k in FEATURES]
+    return input_columns
 
 
 shutil.rmtree(OUTDIR, ignore_errors=True)  # start fresh each time
 
-train_and_evaluate(OUTDIR, num_train_steps=5000)
+model = tf.estimator.LinearRegressor(feature_columns=make_feature_cols(), model_dir=OUTDIR)
+
+model.train(
+    input_fn=make_input_fn(
+        pd.read_csv("./taxi-train.csv", header=None, names=CSV_COLUMNS), num_epochs=100
+    )
+)
+
+
+def print_rmse(model, name, df):
+    metrics = model.evaluate(
+        input_fn=make_input_fn(
+            pd.read_csv("./taxi-valid.csv", header=None, names=CSV_COLUMNS),
+            num_epochs=1,
+        )
+    )
+    print("RMSE on {} dataset = {}".format(name, np.sqrt(metrics["average_loss"])))
+
+
+print_rmse(model, "validation", df_valid)
